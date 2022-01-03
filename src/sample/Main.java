69fd6468b750a26088e429b8f150e164136b4dd6
Giuli <itsmegiuli@gmail.com>
2022-01-03 22:23:29 +0100

    updated

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,12 +12,13 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Connect Four ... by Puzzled");
-        primaryStage.setScene(new Scene(root, 415, 358));
+        primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
