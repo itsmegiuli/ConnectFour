9ac59f23e482872a01cb590127db507a6c15e377
Adam <adam.ibragimov@stud.fh-campuswien.ac.at>
2022-01-08 14:56:30 +0100

    Diagonal Check works!

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -54,17 +54,41 @@ public class Controller extends Shape {
         boolean won = false;
 
         for (int j = 0; j < gameBoard.length; j++) { //horizontally
-            if (gameBoard[j][0]== gameBoard[j][1] &&  gameBoard[j][1] == gameBoard[j][2] && gameBoard[j][2]== gameBoard[j][3] ||
-                    gameBoard[j][1]== gameBoard[j][2] &&  gameBoard[j][2] == gameBoard[j][3] && gameBoard[j][3]== gameBoard[j][4] ||
-                    gameBoard[j][2]== gameBoard[j][3] &&  gameBoard[j][3] == gameBoard[j][4] && gameBoard[j][4]== gameBoard[j][5] ||
-                    gameBoard[j][3]== gameBoard[j][4] &&  gameBoard[j][4] == gameBoard[j][5] && gameBoard[j][5]== gameBoard[j][6]) {
+            if (gameBoard[j][0] == gameBoard[j][1] && gameBoard[j][1] == gameBoard[j][2] && gameBoard[j][2] == gameBoard[j][3] ||
+                    gameBoard[j][1] == gameBoard[j][2] && gameBoard[j][2] == gameBoard[j][3] && gameBoard[j][3] == gameBoard[j][4] ||
+                    gameBoard[j][2] == gameBoard[j][3] && gameBoard[j][3] == gameBoard[j][4] && gameBoard[j][4] == gameBoard[j][5] ||
+                    gameBoard[j][3] == gameBoard[j][4] && gameBoard[j][4] == gameBoard[j][5] && gameBoard[j][5] == gameBoard[j][6]) {
                 won = true;
             }
-            if (gameBoard[0][j]== gameBoard[1][j] &&  gameBoard[1][j] == gameBoard[2][j] && gameBoard[2][j] == gameBoard[3][j] ||
-            gameBoard[1][j]== gameBoard[2][j] &&  gameBoard[2][j] == gameBoard[3][j] && gameBoard[3][j] == gameBoard[4][j] ||
-            gameBoard[2][j]== gameBoard[3][j] &&  gameBoard[3][j] == gameBoard[4][j] && gameBoard[4][j] == gameBoard[5][j]) { //vertically
+            if (gameBoard[0][j] == gameBoard[1][j] && gameBoard[1][j] == gameBoard[2][j] && gameBoard[2][j] == gameBoard[3][j] ||
+                    gameBoard[1][j] == gameBoard[2][j] && gameBoard[2][j] == gameBoard[3][j] && gameBoard[3][j] == gameBoard[4][j] ||
+                    gameBoard[2][j] == gameBoard[3][j] && gameBoard[3][j] == gameBoard[4][j] && gameBoard[4][j] == gameBoard[5][j]) { //vertically
                 won = true;
-            }}
+            }
+        }
+
+        for (int row = 0; row < gameBoard.length - 3; row++) {
+            for (int col = 0; col < gameBoard[row].length - 3; col++) {
+                int element = gameBoard[row][col];
+                if (element == gameBoard[row + 1][col + 1] &&
+                        element == gameBoard[row + 2][col + 2] &&
+                        element == gameBoard[row + 3][col + 3])
+                {
+                    won = true;
+                }
+            }
+        }
+        for (int row = 0; row < gameBoard.length - 3; row++) {
+            for (int col = 3; col < gameBoard[row].length; col++) {
+                int element = gameBoard[row][col];
+                if (element == gameBoard[row + 1][col - 1] &&
+                        element == gameBoard[row + 2][col - 2] &&
+                        element == gameBoard[row + 3][col - 3])
+                {
+                    won = true;
+                }
+            }
+        }
         return won;
     }
 
