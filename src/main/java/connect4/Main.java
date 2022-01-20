e43a10e3f21d1035fb8531586f1ee72c376983a3
kenan <kenan.husic@stud.fh-campuswien.ac.at>
2022-01-20 13:45:41 +0100

    -Optical improvements

--- src/main/java/connect4/Main.java
+++ src/main/java/connect4/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/graphicInterface.fxml"));
-        primaryStage.setTitle("Connect Four ... by Puzzled");
+        primaryStage.setTitle("Connect Four");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
